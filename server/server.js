7a9c6a1ed37baf34179406a96d7a73808be74391
mixersoft <michael@snaphappi.com>
2015-02-20 22:39:44 +0200

    initial commit, see https://github.com/strongloop/loopback-component-storage/tree/master/example-2.0 - added gulp, lodash, etc. see package.json

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname);
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+
+// app.use('/client', loopback.static(__dirname + '/../client'))
+// app.use('/storage', loopback.static(__dirname + '/storage'))
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
+
+
